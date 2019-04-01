af1715b1e08d2796f9c1d21136dc67ea58ceb8c7
Velizar Nenov <vndepot-wdg@yahoo.com>
2019-04-01 23:42:46 +0300

    src init

---
+++ defaults.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * Default values for .env variables and global app vars
+ */
+module.exports = {
+  
+  cacheTtl: 86400, // sec
+  
+  appPort: 5000,
+  
+  host: 'http://localhost'
+};
