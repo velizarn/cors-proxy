7a22569bb554e865dcb059da0f75825f16a1960a
Velizar Nenov <vndepot-wdg@yahoo.com>
2019-04-02 16:20:15 +0300

    new routes and mw

---
+++ src/index.js
@@ -0,0 +1,113 @@
+/* eslint no-unused-vars: 0 */
+/* eslint no-undef: 0 */
+'use strict';
+
+require('dotenv').config();
+
+const defaults = require('./defaults');
+
+const WORKERS = process.env.WEB_CONCURRENCY || require('os').cpus().length;
+
+const {
+  CACHE_TTL = defaults.cacheTtl,
+  LOG_LEVEL = 'error',
+  NODE_ENV,
+  PORT = defaults.appPort,
+  REMOVE_HEADERS = ''
+} = process.env;
+  
+const
+  bodyParser = require('body-parser'),
+  express = require('express'),
+  helmet = require('helmet'),
+  Logger = require('heroku-logger').Logger,
+  path = require('path'),
+  request = require('request-promise'),
+  throng = require('throng');
+
+const logger = new Logger({ level: LOG_LEVEL});
+
+const {
+  middlewareDefaultUri,
+  middlewareTargetUrl,
+  middlewareProxyHeaders,
+  middlewareAmpProxy,
+  middlewareWhitelistDomains
+} = require('./functions');
+ 
+if (NODE_ENV !== 'test') {
+  throng({
+    workers: WORKERS,
+    lifetime: Infinity,
+    master: startMaster,
+    start: startWorker
+  });
+} else {
+  startWorker(1);
+}
+
+// This will only be called once
+function startMaster() {
+  logger.info('Started master');
+}
+
+function startWorker(workerId) {
+  
+  const app = express();
+  
+  app
+    .use(helmet())
+    .use(bodyParser.urlencoded({ extended: false }))
+    .use(bodyParser.json())
+    .use(middlewareDefaultUri)
+    .use(middlewareTargetUrl)
+    .use(middlewareProxyHeaders)
+    .use(middlewareAmpProxy)
+    .use(middlewareWhitelistDomains);
+
+  app.all('*', (req, res) => {
+    
+    let originalHeaders = req.headers;
+    
+    delete originalHeaders.host;
+    
+    request({
+      uri: req.app.locals.requestedUrl,
+      method: req.method,
+      headers: originalHeaders,
+      resolveWithFullResponse: true
+    })
+      .then((response) => {
+      
+        let responseHeaders = response.headers;
+        
+        REMOVE_HEADERS.split(',').forEach((item) => {
+          if (item in responseHeaders) {
+            delete responseHeaders[item.toString()];
+          }
+        });
+        
+        const headers = Object.assign({}, responseHeaders, req.app.locals.proxyHeaders);
+        
+        res.status(response.statusCode);
+        
+        for (var property in headers) {
+          if (headers.hasOwnProperty(property)) {
+            res.set(property, headers[property.toString()]);
+          }
+        }
+        
+        res.send(response.body);
+        res.end();
+      })
+      .catch((err) => {
+        logger.error(err + '');
+        res.status(500);
+        res.send(err + '');
+        res.end();
+      });
+      
+  });
+  
+  module.exports = app.listen(PORT, () => logger.info(`Listening on ${ PORT } / ${workerId}`));
+}
