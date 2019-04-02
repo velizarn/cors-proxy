3ed73ad91feaff498c67b3458c5160773b50ddbe
Velizar Nenov <vndepot-wdg@yahoo.com>
2019-04-02 16:20:15 +0300

    Minor updates and refactoring

---
+++ src/functions.js
@@ -0,0 +1,118 @@
+'use strict';
+
+require('dotenv').config();
+
+const {
+  ADDITIONAL_HEADERS = null,
+  AMP_ALLOWED_ORIGINS = '',
+  AMP_ALLOWED_SOURCE_ORIGIN,
+  AMP_PROXY_ACTOR = 'false',
+  LOG_LEVEL = 'error',
+  WHITELIST_DOMAINS = ''
+} = process.env;
+  
+const
+  crypto = require('crypto'),
+  Logger = require('heroku-logger').Logger,
+  logger = new Logger({ level: LOG_LEVEL});
+
+const hashed = (inputStr) => crypto.createHash('md5').update(inputStr).digest('hex');
+
+const middlewareDefaultUri = (req, res, next) => {
+  if (req.originalUrl === '/') {
+    res.status(200);
+    res.send((new Date()).toLocaleString());
+    res.end();
+    return;
+  }
+  next();
+};
+
+const middlewareMethod = (req, res, next) => {
+  if (req.method.toLowerCase() !== 'get') {
+    res.status(405);
+    res.send('405 Method Not Allowed');
+    res.end();
+    return;
+  }
+  next();
+};
+
+const middlewareTargetUrl = (req, res, next) => {
+  req.app.locals.requestedUrl = `${req.protocol}://${req.originalUrl.replace('/', '')}`;
+  req.app.locals.requestedUrlHashed = `h${hashed(req.app.locals.requestedUrl)}`;
+  return next();
+};
+
+const middlewareProxyHeaders = (req, res, next) => {
+  req.app.locals.proxyHeaders = ADDITIONAL_HEADERS
+    ? JSON.parse(ADDITIONAL_HEADERS)
+    : {};
+  return next();
+};
+
+const middlewareWhitelistDomains = (req, res, next) => {
+  
+  try {
+        
+    const urlObject = (new URL(req.app.locals.requestedUrl));
+        
+    if (WHITELIST_DOMAINS !== '' && !WHITELIST_DOMAINS.includes(urlObject.hostname)) {
+      res.status(400);
+      res.send('Bad Request');
+      res.end();
+      return;
+    }
+          
+  } catch (err) {
+    logger.error(`${err  }`);
+    res.status(400);
+    res.send('Bad Request');
+    res.end();
+    return;
+  }
+
+  next();
+};
+
+const middlewareAmpProxy = (req, res, next) => {
+  
+  if (AMP_PROXY_ACTOR === 'false') {
+    return next();
+  }
+  
+  const unauthorized = 'Unauthorized Request';
+    
+  let origin;
+    
+  const allowedOrigins = AMP_ALLOWED_ORIGINS.replace(', ', ',').split(',');
+    
+  const sourceOrigin = req.query.__amp_source_origin;
+    
+  // If same origin
+  if (req.headers['amp-same-origin'] === 'true') {
+    origin = sourceOrigin;
+    // If allowed CORS origin & allowed source origin
+  } else if (allowedOrigins.indexOf(req.headers.origin) !== -1 &&
+      sourceOrigin === AMP_ALLOWED_SOURCE_ORIGIN) {
+    origin = req.headers.origin;
+  } else {
+    res.statusCode = 401;
+    res.end(JSON.stringify({message: unauthorized}));
+    throw unauthorized;
+  }
+    
+  req.app.locals.proxyHeaders['Access-Control-Allow-Origin'] = origin;
+  req.app.locals.proxyHeaders['AMP-Access-Control-Allow-Source-Origin'] = sourceOrigin;
+  
+  next();
+};
+
+module.exports = {
+  middlewareDefaultUri,
+  middlewareMethod,
+  middlewareTargetUrl,
+  middlewareProxyHeaders,
+  middlewareAmpProxy,
+  middlewareWhitelistDomains
+};
